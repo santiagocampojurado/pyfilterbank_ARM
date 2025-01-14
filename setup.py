b96d3d0459b585da9e80a3d1e7bb41471d11a7f7
Santiago Campo <id00706137@usal.es>
2025-01-14 09:33:40 +0100

    Add sosfilt extension to setup.py and avoid issue with different architectures

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 # -- coding: utf-8 --
 
-from setuptools import setup
+from setuptools import setup, Extension
+
+module_sosfilt = Extension(
+    name="pyfilterbank.sosfilt",
+    sources=["pyfilterbank/sosfilt.c"]
+)
 
 settings = {
     'name': 'pyfilterbank',
-    'version': '0.0.0',
+    ######
+    'version': '0.0.1', # from 0.0.0
+    ######
     'description': 'Filterbanks and filtering for the acoustician and audiologists in python.',
     'url': 'http://github.com/SiggiGue/pyfilterbank',
     'author': u'Siegfried Gündert',
@@ -12,13 +19,16 @@ settings = {
     'license': 'MIT',
     'packages': ['pyfilterbank'],
     'zip_safe': False,
+
     'package_data': {
         'pyfilterbank': [
             'sosfilt.c',
-            'sosfilt64.dll',
-            'sosfilt32.dll',
-            'sosfilt.so'
+            # 'sosfilt64.dll',
+            # 'sosfilt32.dll',
+            # 'sosfilt.so'
         ]
-    }
+    },
+    # extension to setuptools so it will compile it
+    'ext_modules': [module_sosfilt],
 }
 setup(**settings)
